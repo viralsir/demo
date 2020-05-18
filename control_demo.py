ad984207e23359cea122a2dca46accf0c676f5b1
viralsir <viralsir2018@gmail.com>
2020-05-18 16:21:40 +0530

    added three no condition

--- control_demo.py
+++ control_demo.py
@@ -1,7 +1,9 @@
 no1=int(input("Enter No:"))
 no2=int(input("Enter No:"))
-
-if no1>no2:
+no3=int(input("Enter No:"))
+if no1>no2
     print(no1," is a maximum no")
-else :
+elif no2>no3 :
     print(no2," is a maximum no")
+else :
+    print(no3," is a maxium no")
