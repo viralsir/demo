d5e7bba95170efbb6f13fc2b8640605bf29aa64a
viralsir <viralsir2018@gmail.com>
2020-05-18 16:44:40 +0530

    BACK TO THREE NO CONDITION

--- control_demo.py
+++ control_demo.py
@@ -3,7 +3,7 @@ no2=int(input("Enter No:"))
 no3=int(input("Enter No:"))
 
 
-if no1>no2 and no1>no3:
+if no1>no2 and no1>no3 :
     print(no1," is a maximum no")
 elif no2>no3 :
     print(no2," is a maximum no")
