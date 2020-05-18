fb571198c3ee4f4887af6e42922bb542ef06b0bf
viralsir <viralsir2018@gmail.com>
2020-05-18 16:39:59 +0530

    revert the changes

--- control_demo.py
+++ control_demo.py
@@ -1,11 +1,7 @@
 no1=int(input("Enter No:"))
 no2=int(input("Enter No:"))
-no3=int(input("Enter No:"))
 
-
-if no1>no2 and no1>no3:
+if no1>no2:
     print(no1," is a maximum no")
-elif no2>no3 :
-    print(no2," is a maximum no")
 else :
-    print(no3," is a maxium no")
+    print(no2," is a maximum no")
